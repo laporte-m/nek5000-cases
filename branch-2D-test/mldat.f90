0195a7826dc7d54abd1ef1d0d229f4c13abba74d
Matt Laporte <Matt.L.Laporte@gmail.com>
2015-05-08 12:52:41 -0400

    Creating .usr file for Rym, 2D water-0.3% polyox case with updated mesh. Starting to add BC storage for one-time calculation

---
+++ branch-2D-test/mldat.f90
@@ -0,0 +1,18 @@
+      module mldat
+      
+       IMPLICIT NONE
+
+       SAVE
+       
+       real :: tsc, lsc, msc
+       real,dimension(:,:,:,:),allocatable :: shvis
+       real,dimension(:,:,:,:),allocatable :: uxbc, uybc
+       real :: n1, n2, udiff1, udiff2
+       real :: um1, um2
+       real :: rr1, rr2, yy1, yy2
+       real :: th, thi, xbr 
+       real :: rr3, mid
+       real :: nre1, nre2 
+       real :: gm = 1e38
+	    
+      end module mldat	  
