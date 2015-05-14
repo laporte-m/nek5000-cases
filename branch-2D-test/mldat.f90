005b292695486cd6aadc3fbcc9abad20366066a3
Matt Laporte <Matt.L.Laporte@gmail.com>
2015-05-13 21:57:35 -0400

    Added dfc to mldat to fix problems with passive scalar

--- branch-2D-test/mldat.f90
+++ branch-2D-test/mldat.f90
@@ -8,12 +8,11 @@
        real,dimension(:,:,:,:),allocatable :: shvis
        real,dimension(:,:,:,:),allocatable :: uxbc, uybc
        real,dimension(2,2) :: mcnr, tcnr, bcnr
-       real :: n1, n2, udiff1, udiff2, cnc1, cnc2
-       real :: um1, um2
-       real :: rr1, rr2, yy1, yy2
-       real :: th, thi, xbr 
-       real :: rr3, mid
+       real :: n1, n2, udiff1, udiff2, cnc1, cnc2, dfc
        real :: nre1, nre2 
+       real :: um1, um2
+       real :: rr1, rr2, rr3, mid, yy1, yy2
+       real :: th, thi 
        real :: gm = 1e38
 	    
       end module mldat	  
